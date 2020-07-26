a42552e76e08407ce98f54aaed41cc193a794545
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-07-26 21:51:44 +0530

    added event handler to change outline of menu to countrycode

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState(["worldwide"]);
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -19,12 +20,19 @@ function App() {
     };
     getCountriesData();
   }, [countries]);
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    console.log(countryCode);
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
       <div className="app_header">
         <h1>Covid-19 Tracker</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
