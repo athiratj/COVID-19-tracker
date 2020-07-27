616fdf7b7ce6bf10e72ad420b8c9808c9e45b951
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-07-27 21:13:36 +0530

    added table component with live cases

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import {
 import "./App.css";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
+import Table from "./Table";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState(["worldwide"]);
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -32,6 +34,7 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }));
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -96,6 +99,7 @@ function App() {
       <Card className="app_right">
         <CardContent>
           <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
           <h3>Worldwide new Cases</h3>
         </CardContent>
       </Card>
---
+++ src/Table.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Table({ countries }) {
+  return (
+    <div className="table">
+      {countries.map(({ country, cases }) => (
+        <tr>
+          <td>{country}</td>
+          <td>
+            <strong>{cases}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
+}
+
+export default Table;
