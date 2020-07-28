c52d6c42bc8ef6d3046f52834a9c654dc6854ca6
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-07-28 21:00:43 +0530

    added functionality to loop through the map and draw circles on the screen

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import "./Map.css";
+import { showDataOnMap } from "./util";
 
-function Map({ countries, center, zoom }) {
+function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
       <LeafletMap center={center} zoom={zoom}>
@@ -10,7 +11,7 @@ function Map({ countries, center, zoom }) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>contributors'
         />
-        {}
+        {showDataOnMap(countries, casesType)}
       </LeafletMap>
     </div>
   );
