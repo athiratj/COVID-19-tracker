b97e3e34ad4cd9e4c0d31ae58f81582e65425eac
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-07-28 21:51:38 +0530

    added styling to InfoBoxes

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
 
 function InfoBox({ title, cases, total }) {
   return (
-    <div>
-      <Card className="infoBox">
-        <CardContent>
-          <Typography className="infoBox_title" color="textSecondary">
-            {title}
-          </Typography>
-          <h2 className="infoBox_cases">{cases}</h2>
-          <Typography className="infoBox_total">{total} Total</Typography>
-        </CardContent>
-      </Card>
-    </div>
+    <Card className="infoBox">
+      <CardContent>
+        <Typography className="infoBox_title" color="textSecondary">
+          {title}
+        </Typography>
+        <h2 className="infoBox_cases">{cases}</h2>
+        <Typography className="infoBox_total">{total} Total</Typography>
+      </CardContent>
+    </Card>
   );
 }
 
