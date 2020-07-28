99ffb3e4684658e52ea6e9d6da63c40e846ba679
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-07-28 10:51:57 +0530

    installed numeral for graph and did somr fetching of data

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,21 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
+import numeral from "numeral";
 
-function LineGraph() {
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+};
+
+const buildChartData = (data, casesType = "cases") => {
+  const chartData = [];
+  let lastDataPoint;
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+function LineGraph({ casesType = "cases" }) {
   const [data, setData] = useState({});
 
   useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+          let chartData = buildChartData(data, "cases");
+          setData(chartData);
+        });
+    };
+    fetchData();
   }, []);
 
   return (
     <div>
       <h1>I am a graph</h1>
-      <Line />
+      {data?.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204, 16, 52, 0.5)",
+                borderColor: "#cc1034",
+                data: data,
+              },
+            ],
+          }}
+        />
+      )}
     </div>
   );
 }
