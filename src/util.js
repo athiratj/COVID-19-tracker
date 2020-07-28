445c7e0b767523677fc6d0680494a0ebf7c81cfb
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-07-28 21:18:41 +0530

    added styling to the popup info

--- src/util.js
+++ src/util.js
@@ -43,14 +43,21 @@ export const showDataOnMap = (data, casesType = "cases") =>
       }
     >
       <Popup>
-        <div>
+        <div className="info-container">
           <div
+            className="info-flag"
             style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
           />
-          <div>{country.country}</div>
-          <div>Cases: {numeral(country.cases).format("0,0")}</div>
-          <div>Recovered: {numeral(country.recovered).format("0,0")}</div>
-          <div>Deaths: {numeral(country.deaths).format("0,0")}</div>
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
         </div>
       </Popup>
     </Circle>
